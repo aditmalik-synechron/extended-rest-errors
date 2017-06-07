f6b7c57b3091f2bde48b84d1de0ec660c82a4a1c
Jagdish <jagdish.idhate@synechron.com>
2017-06-07 16:24:29 +0530

    Error stack update

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const httpStatusCodes = require('./status-codes.json');
 Object.keys(httpStatusCodes).forEach(key => {
-  exports[key] = function (options, causedBy) {
-    this.errorResponse = Object.assign({
-      'packageName': process.env.NODE_PACKAGE_NAME || 'unknown',
-      'version': process.env.NODE_PACKAGE_VERSION || 'unknown',
-      'category': '',//TBD
-      'path': '',//TBD
-      'causedBy': causedBy || {},
-      'code': httpStatusCodes[key].code,
-      'exception': key,
-      'message': httpStatusCodes[key].message,
-      'source': process.env.NODE_ENV === 'production' ? '' : this.stack,
-      'status': httpStatusCodes[key].status,
-      'timestamp': Date.now().toString(),
-      'validationErrors': []
-    }, options);
-  };
-  exports[key].prototype = new Error;
+  exports[key] = class extends Error {
+    constructor(options, causedBy) {
+      super();
+      this.name = this.constructor.name;
+      if (typeof Error.captureStackTrace === 'function') {
+        Error.captureStackTrace(this, this.constructor);
+      } else {
+        this.stack = (new Error()).stack;
+      }
+      this.errorResponse = Object.assign({
+        'packageName': process.env.NODE_PACKAGE_NAME || 'unknown',
+        'version': process.env.NODE_PACKAGE_VERSION || 'unknown',
+        'category': '',//TBD
+        'path': '',//TBD
+        'causedBy': causedBy || {},
+        'code': httpStatusCodes[key].code,
+        'exception': key,
+        'message': httpStatusCodes[key].message,
+        'source': process.env.NODE_ENV === 'production' ? '' : this.stack,
+        'status': httpStatusCodes[key].status,
+        'timestamp': Date.now().toString(),
+        'validationErrors': []
+      }, options);
+    }
+  }
 });
 exports.init = function init(options) {
   options = options || {};
@@ -31,3 +39,4 @@ exports.status = function (code,options,causedBy) {
   }
   return exports.InternalServerError;
 };
+
